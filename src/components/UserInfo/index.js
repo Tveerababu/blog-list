541d92c474979837101bb3c02313535f9e6928a2
Tveerababu <babuv9985@gamil.com>
2023-05-18 11:20:52 +0000

    blog-list

---
+++ src/components/UserInfo/index.js
@@ -0,0 +1,17 @@
+// Write your JS code here
+// Write your JS code here
+import './index.css'
+
+const UserInfo = () => (
+  <div className="user-profile-container">
+    <img
+      alt="profile"
+      src="https://assets.ccbp.in/frontend/react-js/profile-img.png"
+      className="user-image"
+    />
+    <h1 className="user-title">Wade Warren</h1>
+    <p className="user-description">Software developer at UK</p>
+  </div>
+)
+
+export default UserInfo
